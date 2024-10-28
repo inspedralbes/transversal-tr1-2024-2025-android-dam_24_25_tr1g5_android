dead3a520989b516279c56198699f1ddf9a98ec6
Fabianroldan <a21fabrolfer@inspedralbes.ca>
2024-10-28 13:18:54 +0100

    Login screen

---
+++ AppTakeAway/app/src/main/java/com/example/apptakeaway/InitialScreenActivity.kt
@@ -0,0 +1,25 @@
+package com.example.apptakeaway
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
+class InitialScreenActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_initialscreen) // Establece el layout de la pantalla inicial
+
+
+
+        val guestButton = findViewById<Button>(R.id.guestButton)
+        guestButton.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish() //
+        }
+    }
+}
